abc1015ce609993dd7a189a8b5fbc69b7f4ad348
EbertCC <165855506+EbertCC@users.noreply.github.com>
2024-04-09 19:44:56 -0500

    Add files via upload

---
+++ numero_palindromo_con_clases.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+
+class palin{
+private:
+	int numero;
+public:
+	explicit palin(int num){
+		numero = num;
+	}
+	
+	void set_numero(int num){
+		numero = num;
+	}
+	int get_numero()const{
+		return numero;
+	}
+	
+	bool numero_es_palin()const{
+		
+		int numero_pa = numero;
+		int numero_ori = numero;
+		int numero_revertido = 0;
+		int ultimo_digito;
+		
+		while(numero_pa != 0){
+			ultimo_digito = numero_pa%10;
+			
+			numero_revertido = numero_revertido*10 + ultimo_digito; 
+			
+			numero_pa = numero_pa/10;
+		}
+		if(numero_ori == numero_revertido){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+};
+
+int main(){
+	int num;
+	palin numero1(5);
+	//cout<< numero1.get_numero()<<endl;
+	
+	cout<<"ingre numero : "<<endl;
+	cin>> num ;
+	numero1.set_numero(num);
+	//cout<< numero1.get_numero();
+	
+	
+	if(numero1.numero_es_palin() == true ){
+		cout<<"es palin"<<endl;
+	} else {
+		cout<<"no es palin"<<endl;
+	}
+	
+	return 0;
+}
