a6e43fe20a72db717f4727213d147623c866515a
EbertCC <165855506+EbertCC@users.noreply.github.com>
2024-04-18 08:25:23 -0500

    Add files via upload

--- hola.cpp
+++ hola.cpp
@@ -1,49 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int potencia(int a, int b){
-	int cont = 1;
-	for(int i=0 ; i<b;i++){
-		cont = cont * a;
-	}
-	return cont;
-}
-
-bool esprimo(int a){
-    int cont = 0;
-    for(int i=1;i<=a;i++){
-        if(a%i == 0){
-            cont = cont + 1;
-        }
-    }
-    if(cont == 2){
-        return true;
-    } else {
-        return false;
-    }
-
-
-}
-
-void imprimir_nprimps(int a){ // n<100
-    int cont = 0;
-	for(int i=0; i<100; i++){
-		if(esprimo(i) == true){
-			cont = cont + 1;
-			cout<<i<<" , ";
-		}
-		if(cont == a){
-			break;
+bool esPrimo(int n) {
+	if( n % 2 == 0 || n == 0 || n == 1) return false;
+	for(int i = 2; i <= n/2; i++){
+		if( n % i == 0){
+			return false;
 		}
 	}
+	return true;
 }
 
+/*
+* Implemente una función que reciba un entero n e imprima
+* los n primeros nros primos.
+
+void printNPrimos(int n) {
+	
+	contador = 1
+	MIENTRAS contador < n
+	SI contador es primo
+	imprime contador    
+	contador = contador + 1        
+	
+}
+*/
 void imprimir_nprimps2(int a){// n<infinito
 	int cont1 = 1;
 	int cont2 = 0;
 	
 	while(cont1 <= a){
-		if(esprimo(cont2) == true){
+		if(esPrimo(cont2) == true){
 			cout<<cont2<<" , ";
 			cont1 = cont1 + 1; 
 		}
@@ -51,13 +38,91 @@ void imprimir_nprimps2(int a){// n<infinito
 	} 
 }
 
-int main(int argc, char *argv[]) {
-	
-	//cout<<"potencia (3,4)"<<potencia(3,4);
-	
-    //cout<<"es pirmo el : "<<esprimo(7);
-    
-	imprimir_nprimps2(10);
+/*
+//escrib una funcion que rsiva cadena y retorne string
+int tam_cade(string cad){
 
+	int counter{0};
+    for( int i = 0; i < cad.length()  ; i++ ) {                
+        int val = static_cast<int>(cad.at(i));
+        if(val >= 48 && val <= 59)
+            counter++;
+    }
+	return counter;
+}
+*/
+
+int cont_cadena(string cad){
+	int counter{0};
+	for( int i = 0; i < cad.length()  ; i++ ) {                
+		int val = static_cast<int>(cad.at(i));
+		if(val >= 97 && val <= 122)
+			counter++;
+	}
+	return counter;
+}
+	
+void conver_cadena(string cad){
+	char c;
+	
+	for( int i = 0; i < cad.size(); i++ ) {        
+		c = cad.at(i);
+		int val = static_cast<int>(c);
+		if(val >= 97 && val <= 122)
+			cad.at(i) = c - 32;  //toupper(c);
+	}
+	
+	cout<<"Cdenaa mayucula : "<<cad;
+}
+
+void fibo(int num){
+	int temp1 = 1;
+	int temp2 = 1;
+	int temp3 = 1;
+
+	for(int i = 1; i<=num;i++){
+
+		if(i==1){
+			//temp3 = 1;
+			continue;
+		}
+		
+
+		temp3 = temp1 + temp2;
+		temp1 = temp2;
+		temp2 = temp3;
+	}
+	cout<<temp3;
+
+
+}
+
+int re_fibo(int n){
+	if(n==1 || n ==0){
+		return 1;
+	} else {
+		return re_fibo(n-2) + re_fibo(n-1);
+	}
+}
+
+int main(){
+	// 0 1 1 2 3 5 8
+	long long numero;
+	bool noEsPrimo = false;
+	/*
+	cout<<"Introduce el numero: ";cin>>numero;
+	
+	if( esPrimo(numero) ) {
+		cout << numero << " ES PRIMO" << endl;
+	}else{
+		cout << numero << " NO ES PRIMO" << endl;
+	}
+	*/
+	
+	//imprimir_nprimps2(10);
+	//cout<<cont_cadena("hola");
+	//conver_cadena("hola");
+    fibo(5);
+	cout<<re_fibo(5);
 	return 0;
 }
